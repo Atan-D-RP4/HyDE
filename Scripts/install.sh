81fee03b82cd4e8432b095674847511cfa9f3551
Chaitanya Dubakula <chaitanyarp4@gmail.com>
2025-06-29 02:06:53 +0530

    (patch): modified aur helper priority and kitty conf compatibility

--- Scripts/install.sh
+++ Scripts/install.sh
@@ -73,7 +73,7 @@ Usage: $0 [options]
             m : no the[m]e reinstallations
             t : [t]est run without executing (-irst to dry run all)
 
-NOTE: 
+NOTE:
         running without args is equivalent to -irs
         to ignore nvidia, run -irsn
 
@@ -170,18 +170,18 @@ EOF
         prompt_timer 120 "Enter option number [default: yay-bin] | q to quit "
 
         case "${PROMPT_INPUT}" in
-        1) export getAur="yay" ;;
-        2) export getAur="paru" ;;
-        3) export getAur="yay-bin" ;;
-        4) export getAur="paru-bin" ;;
+        1) export getAur="paru-bin" ;;
+        2) export getAur="yay-bin" ;;
+        3) export getAur="paru" ;;
+        4) export getAur="yay" ;;
         q)
             print_log -sec "AUR" -crit "Quit" "Exiting..."
             exit 1
             ;;
         *)
-            print_log -sec "AUR" -warn "Defaulting to yay-bin"
-            print_log -sec "AUR" -stat "default" "yay-bin"
-            export getAur="yay-bin"
+            print_log -sec "AUR" -warn "Defaulting to paru-bin"
+            print_log -sec "AUR" -stat "default" "paru-bin"
+            export getAur="paru-bin"
             ;;
         esac
         if [[ -z "$getAur" ]]; then
